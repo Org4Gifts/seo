d6c035c05c7a6fa1d7884602b01e3bafcb23a411
Odise <odise9411272@gmail.com>
2020-03-12 00:43:39 +0800

    complete search and click.

--- src/main/java/com/adima/seo/util/HttpClientUtil.java
+++ src/main/java/com/adima/seo/util/HttpClientUtil.java
@@ -8,12 +8,12 @@ package com.adima.seo.util;
 import java.io.IOException;  
 import org.apache.commons.httpclient.Cookie;
 
-
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.client.methods.HttpGet;
 //import org.apache.commons.lang.xwork.StringUtils;
 
 /**
@@ -45,6 +45,7 @@ public class HttpClientUtil {
     String rest = null;  
     if(proxyHost != null && proxyPort != 0) httpClient.getHostConfiguration().setProxy(proxyHost, proxyPort);  
     HttpMethod method = new GetMethod(url);  
+    HttpGet get = new HttpGet(url);
     if(!StringUtils.isBlank(cookie)) {  
       method.addRequestHeader("Cookie", cookie);  
     }  
@@ -52,7 +53,38 @@ public class HttpClientUtil {
     //Mozilla/5.0 (Windows NT 6.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1  
     httpClient.executeMethod(method);  
     
-    //檢查cookie
+    //檢查cookie 確認 cookie 不會重複
+//    Cookie[] cookies = httpClient.getState().getCookies();
+//    for(int i=0; i<cookies.length; i++){
+//        System.out.println("cookies["+i+"]: "+cookies[i]);
+//    }
+  
+    //根据http头解析正确的字符集  
+    String header = method.getResponseHeader("Content-Type").getValue();  
+    if(header.contains("charset=")) {  
+      encoding = header.substring(header.indexOf("charset=") + "charset=".length(), header.length());  
+    }  
+    if(encoding == null) encoding = "GBK";  
+  
+    rest = new String(method.getResponseBody(), encoding);  
+    method.releaseConnection();  
+    return rest;  
+  }
+
+public static String getHtmlBySearch(String url, int port, String encoding, String proxyHost, int proxyPort, String cookie)  
+      throws HttpException, IOException {  
+    HttpClient httpClient = getClient();  
+    String rest = null;  
+    if(proxyHost != null && proxyPort != 0) httpClient.getHostConfiguration().setProxy(proxyHost, proxyPort);  
+    HttpMethod method = new GetMethod(url);  
+    if(!StringUtils.isBlank(cookie)) {  
+      method.addRequestHeader("Cookie", cookie);  
+    }  
+    method.addRequestHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 2.0.50727");  
+    //Mozilla/5.0 (Windows NT 6.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1  
+    httpClient.executeMethod(method);  
+    
+    //檢查cookie 確認 cookie 不會重複
 //    Cookie[] cookies = httpClient.getState().getCookies();
 //    for(int i=0; i<cookies.length; i++){
 //        System.out.println("cookies["+i+"]: "+cookies[i]);
@@ -72,7 +104,11 @@ public class HttpClientUtil {
   
   public static void main(String[] args) throws HttpException, IOException {  
 //    String url = "http://blog.csdn.net/xmlrequest/article/details/11519503";  
-    String url = "http://www.google.com"; 
-    System.out.println(getHtml(url));  
+    String url = "https://www.google.com.tw/search?q=adima"; 
+//    String url = "http://www.google.com.tw/search?q=adima"; 
+//    System.out.println(getHtml(url)); 
+    String newUrl = new AnalyUrl().analyUrl(getHtml(url));
+    
+        System.out.println(getHtml(newUrl)); 
   }  
 }  
